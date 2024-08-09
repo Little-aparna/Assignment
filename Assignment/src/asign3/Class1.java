c150b6431be4fab019ee7fcba63a9d43c243fa38
Little-aparna <aparnaks991@gmail.com>
2024-08-09 20:05:49 +0530

    commit

---
+++ Assignment/src/asign3/Class1.java
@@ -0,0 +1,11 @@
+package asign3;
+
+public class Class1 {
+	public int bas=20000;
+	public int ded=5000;
+	public int bonus=2000;
+	
+	public double hra=(.05*bas);
+	public double pf=(.2*bas);
+	public double total=bas+hra-ded+bonus+pf;
+}
